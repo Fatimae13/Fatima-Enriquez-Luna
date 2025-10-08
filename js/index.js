6dfb9f102c55b6963d229c9a17edf085b5789df4
Fatimae13 <212909285+Fatimae13@users.noreply.github.com>
2025-10-07 22:15:31 -0500

    form and functionality added

---
+++ js/index.js
@@ -0,0 +1,74 @@
+const body = document.body;
+
+// footer
+
+let footer = document.createElement("footer");
+ body.appendChild(footer);
+
+ const today = new Date();
+ const thisYear = today.getFullYear();
+
+
+const copyright = document.createElement("p");
+copyright.innerHTML = `\u00A9 Fatima Enriquez ${thisYear}`;
+footer.appendChild(copyright);
+
+footer.style.textAlign = "center"; 
+
+
+///---- skills----
+const skills= ["JavaScript", "HTML", "CSS", "Git", "GitHub"];
+
+const skillsSelection = document.getElementById("Skills");
+const skillsList = skillsSelection.querySelector("ul");
+
+for(let i=0; i< skills.lenght; i++) {
+    const skill= document.createElement("li");
+    skill.innerText = skill[i];
+    skill.appendChild(skills);
+
+}
+
+////--- Message form-----
+
+const messageForm = document.forms["leave_messages"];
+
+messageForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+
+    const userName = event.target.usersName.value;
+    const userEmail = event.target.usersEmail.value;
+    const userMessage= event.target.usersMessage.value;
+
+    console.log("Name: ", userName);
+    console.log("Email: ", userEmail);
+    console.log("Message: ", userMessage);
+
+
+
+    const messageSection = document.getElementById("Messages");
+
+    const messageList = messageSection.querySelector("ul");
+
+    const newMessage = document.createElement("li");
+
+    newMessage.innerHTML = `<a href="mailto:${userEmail}">${userName}</a>: <span>${userMessage}</span>`;
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.className = "remove-btn";
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click", function() {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
+
+
+    messageForm.reset();
+});
